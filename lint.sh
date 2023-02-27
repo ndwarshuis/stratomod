2039069cf3c3408ad3318d3a2c446e3f9154cf1e
ndwarshuis <ndwar@yavin4.ch>
2023-02-27 15:39:05 -0500

    ADD linter steps to pipeline

--- lint.sh
+++ lint.sh
@@ -1,35 +1,48 @@
 #! /bin/bash
 
+# lint all scripts
+
+# usage: lint.sh [ENV_BASE_PREFIX]
+#
+# where ENV_BASE_PREFIX is an optional location to store each environment
+# (meant to be used by the CI/CD pipeline) and should correspond to what was
+# used to create the environments using `setup_dev.sh`
+
 eval "$(${CONDA_EXE} shell.bash hook 2> /dev/null)"
 anyfailed=false
+if [ -z "$1" ]; then
+    prefix="$1"
+fi
 
 _mypy () {
+    # run mypy without the cache to ensure a clean (albeit slow) lint
     mypy --no-incremental --cache-dir=/dev/null "$1"
 }
 
-echo "Testing bed scripts"
-echo ""
+_conda_activate () {
+    # use either a local env or a user-installed env
+    base="$1"
+    if [ -z "$prefix" ]; then
+        conda activate stratomod-"$base"
+    else
+        conda activate -p "$prefix-$base"
+    fi
+}
 
-conda activate stratomod-bedtools
+for base in bedtools ebm; do
+    echo "Testing scripts for env: $base"
+    echo ""
+    
+    _conda_activate "$base"
+    
+    flake8 "workflow/scripts/python/$base"
+    anyfailed=$([ $? -ne 0 ] || $anyfailed)
+    
+    _mypy "workflow/scripts/python/$base"
+    anyfailed=$([ $? -ne 0 ] || $anyfailed)
 
-flake8 workflow/scripts/python/bedtools
-anyfailed=$([ $? -ne 0 ] || $anyfailed)
-
-_mypy workflow/scripts/python/bedtools
-anyfailed=$([ $? -ne 0 ] || $anyfailed)
-
-echo ""
-
-echo "Testing EBM scripts"
-echo ""
-
-conda activate stratomod-ebm
-
-flake8 workflow/scripts/python/ebm
-anyfailed=$([ $? -ne 0 ] || $anyfailed)
-
-_mypy workflow/scripts/python/ebm
-anyfailed=$([ $? -ne 0 ] || $anyfailed)
+    echo ""
+done
 
 if $anyfailed; then
    exit 1
--- setup_dev.sh
+++ setup_dev.sh
@@ -2,10 +2,21 @@
 
 # Install all development envs for python code. Assumes mamba is installed
 
+# usage: setup_dev.sh [ENV_BASE_PREFIX]
+#
+# where ENV_BASE_PREFIX is an optional location to store each environment
+# (meant to be used by the CI/CD pipeline)
+
 for i in workflow/scripts/{python,rmarkdown}/*; do
     base="$(basename "$i")"
     if [ "$base" != "common" ]; then
-        mamba env update -f "$i/env.yml" -n "stratomod-$base" --prune
+        if [ -z "$1" ]; then
+            location=("-n" "stratomod-$base")
+        else
+            location=("-p" "$1-$base")
+        fi
+        mamba env update -f "$i/env.yml" "${location[@]}" --prune
+        
     fi
 done
 
